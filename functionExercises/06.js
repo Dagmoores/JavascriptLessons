f9418ec8331f92414005abe561a7528a5522889d
Daniel Moore <dagmoores@gmail.com>
2021-02-11 10:29:45 -0300

    Create 06.js

---
+++ functionExercises/06.js
@@ -0,0 +1,13 @@
+/*
+06) Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A
+primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
+retornará o valor da aplicação sob o regime de juros compostos.
+*/
+
+//SimpleInterest
+function simpleInterest (bankStock, interest, time) {
+    result = bankStock + (bankStock * (interest * time))
+    console.log(result.toFixed(2))
+}
+
+simpleInterest(1000, 0.1, 6)
