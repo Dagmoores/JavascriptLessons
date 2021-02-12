72abefbf6368254a295e1fce478b0373fa79f24a
Daniel Moore <dagmoores@gmail.com>
2021-02-12 15:57:05 -0300

    Create 08.js

---
+++ functionExercises/08.js
@@ -0,0 +1,18 @@
+/*
+08) Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
+registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
+mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
+pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+jogo. (Número do pior jogo).
+*/
+
+let values = "10, 20, 30, 40, 50, 60, 70, 80"
+
+function toCompare (values) {
+    let separedValues = values.split(",")
+
+    let separedValuesTest = separedValues
+}
+
+console.log(toCompare())
