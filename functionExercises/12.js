c4329911944bf30678a95acadadb5a53382a4060
Daniel Moore <dagmoores@gmail.com>
2021-02-17 19:14:18 -0300

    Update 12.js

--- functionExercises/12.js
+++ functionExercises/12.js
@@ -2,9 +2,11 @@
 12) Faça um algoritmo que calcule o fatorial de um número.
 */
 
+
 function factorialCalc (x) {
     for (i = 1; i != x; i++){
-        return console.log('o fatorial de', x, 'é', (i * i++)) 
+        let result = (i * i++)
+        return console.log('The result of', x,'factorial', 'is') 
     }
 }
 
