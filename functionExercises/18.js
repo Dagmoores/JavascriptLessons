9b8d1cdd2084a51c3488c74e759baa651d089434
Daniel Moore <dagmoores@gmail.com>
2021-02-22 20:12:28 -0300

    Create 18.js

---
+++ functionExercises/18.js
@@ -0,0 +1,54 @@
+/*
+18) Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando
+switch. Crie um case default que escreva ‘Número fora do intervalo.’
+*/
+
+function numberWriter (number) {
+    switch (number) {
+        case 0: 
+            console.log('Zero')
+            break;
+        case 1:
+            console.log('One')
+            break;
+        case 2:
+            console.log('Two')
+            break;
+        case 3:
+            console.log('Three')
+            break;
+        case 4:
+            console.log('Four')
+            break;
+        case 5:
+            console.log('Five')
+            break;
+        case 6:
+            console.log('Six')
+            break;
+        case 7:
+            console.log('Seven')
+            break;
+        case 8:
+            console.log('Eight')
+            break;
+        case 9:
+            console.log('Nine')
+            break;
+        case 10:
+            console.log('Ten')
+            break;
+    }
+}
+
+numberWriter(0)
+numberWriter(1)
+numberWriter(2)
+numberWriter(3)
+numberWriter(4)
+numberWriter(5)
+numberWriter(6)
+numberWriter(7)
+numberWriter(8)
+numberWriter(9)
+numberWriter(10)
