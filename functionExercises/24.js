712c4d242c3e638e335134c9580c5a687db0218a
Daniel Moore <dagmoores@gmail.com>
2021-03-22 12:22:25 -0300

    Create 24.js

---
+++ functionExercises/24.js
@@ -0,0 +1,11 @@
+/*
+24) Crie um programa que imprima 11 vezes a frase " Hello World!"
+utilizando uma estrutura de repetição while.
+*/
+
+a = 0
+
+while (a < 12) {
+  console.log("Hello, World");
+  a++
+}
