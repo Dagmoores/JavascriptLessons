582dbbd8362d30c427ecccb257f9ae679843dbc3
Daniel Moore <dagmoores@gmail.com>
2021-03-22 12:44:30 -0300

    Create 27.js

---
+++ functionExercises/27.js
@@ -0,0 +1,19 @@
+/*
+27) Construa uma função que receba como parâmetros as alturas e as taxas 
+de crescimento anuais de duas crianças e calcule se existe uma criança 
+menor, caso exista se a criança menor ultrapassará a maior e em quantos
+anos isso acontecerá. Utilize centímetros para as unidades de medida.
+*/
+
+let ChildA = {
+  height: 1.10,
+  growthRate: 0.10
+}
+
+let ChildB = {
+  height: 1.00,
+  growthRate: 0.110
+}
+
+function heightCalc (height, growthRate) {
+}
