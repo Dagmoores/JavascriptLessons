f7d7380a5519baa57c573600e81f68301fd1deb6
Daniel Moore <dagmoores@gmail.com>
2021-03-22 13:00:02 -0300

    Update 27.js

--- functionExercises/27.js
+++ functionExercises/27.js
@@ -7,24 +7,35 @@ anos isso acontecerá. Utilize centímetros para as unidades de medida.
 
 let childA = {
   height: 1.10,
-  growthRate: 0.10
+  growthRate: 0.10,
+  age: 11
 }
 
 let childB = {
   height: 1.00,
-  growthRate: 0.110
+  growthRate: 0.110,
+  age: 8
 }
 
-function heightCalc (height, growthRate) {
+function heightCalc (heightA, heightB, growthRateA, growthRateB) {
   
     heightA = childA.height
     heightB = childB.height
+    growthRateA = childA.growthRate
+    growthRateB = childB.growthRate
 
-   if (heightA > heightB) {
+    if (heightA > heightB && growthRabeB < growthRateA) {
      console.log("The Child A is Bigger than the child B")
+    }
+    else if (heightA > heightB && growthRateB > growthRateA) {
+     console.log("The Child A is Bigger than the child B")
+     for (i = heightB; //stopped here) )
+     
    } else {
      console.log("The Child B is Bigger than the child A")
    }
+
+   
   
 }
 
