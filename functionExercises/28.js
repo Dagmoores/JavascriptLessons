7d8d0af43dda1acd8eb0d5905352514aab2a10b0
Daniel Moore <dagmoores@gmail.com>
2021-04-12 12:44:28 -0300

    Create 28.js

---
+++ functionExercises/28.js
@@ -0,0 +1,12 @@
+/* 28) Ler um vetor de números inteiros e 
+imprimir quantos são pares e quantos são ímpares. */
+
+let vector = [1, 32, 69, 54, 87, 23, 12, 86, 87]
+
+for (let i = 0; i < vector.length ; i++) {
+
+  if (vector[i] % 2 != 0) {
+    
+  }
+  
+}
