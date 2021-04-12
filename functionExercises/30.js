f108804e9e8862eefa2e39d7bdee5c96f072c538
Daniel Moore <dagmoores@gmail.com>
2021-04-12 13:47:13 -0300

    Update 30.js

--- functionExercises/30.js
+++ functionExercises/30.js
@@ -3,7 +3,7 @@ o maior e menor valor dentro do vetor. */
 
 let vector = [1, 23, 12, 5, 76, 12, 22, 5, 4, 3, 1, 2, 9, 34, 75]
 let higher = null
-let lowest = null
+let lowest = Infinity
 
 for (i = 0; i < vector.length; i++) {
   if (vector[i] > higher) {
