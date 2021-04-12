a5bd1f7a63436cdcd43650688c1a73069b66d015
Daniel Moore <dagmoores@gmail.com>
2021-04-12 13:16:49 -0300

    Update 28.js

--- functionExercises/28.js
+++ functionExercises/28.js
@@ -2,11 +2,17 @@
 imprimir quantos são pares e quantos são ímpares. */
 
 let vector = [1, 32, 69, 54, 87, 23, 12, 86, 87]
+let pair = []
+let odd = []
 
 for (let i = 0; i < vector.length ; i++) {
 
   if (vector[i] % 2 != 0) {
-    
+    pair.push(vector[i])
+  } else {
+    odd.push(vector[i])
   }
-  
 }
+
+console.log(pair)
+console.log(odd)
