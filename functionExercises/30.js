e8dc03fe9fae996f15bc11ee6c1d4d9c0a6fe0ae
Daniel Moore <dagmoores@gmail.com>
2021-04-12 13:45:25 -0300

    Create 30.js

---
+++ functionExercises/30.js
@@ -0,0 +1,17 @@
+/* 30) Escreva um algoritmo que percorre um vetor de inteiros e defina 
+o maior e menor valor dentro do vetor. */
+
+let vector = [1, 23, 12, 5, 76, 12, 22, 5, 4, 3, 1, 2, 9, 34, 75]
+let higher = null
+let lowest = null
+
+for (i = 0; i < vector.length; i++) {
+  if (vector[i] > higher) {
+    higher = vector[i]
+  } else if (vector[i] < lowest) {
+   lowest = vector[i] 
+  } else {
+  }
+}
+console.log("Higher number: ", higher)
+console.log("Lowest number: ", lowest)
