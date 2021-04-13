d42f716077dda1fbb3df8af38f80ba5217bd7aeb
Daniel Moore <dagmoores@gmail.com>
2021-04-13 17:29:11 -0300

    Create 32.js

---
+++ functionExercises/32.js
@@ -0,0 +1,14 @@
+/* 32) Construir um algoritmo que calcule a média aritmética dos valores 
+de um vetor de inteiros. */ 
+
+let vector = [1, 3, 8, 10, 12, 89]
+let media
+let result
+
+function mediaCalc (vector[i]) {
+  for (i = 0; i < vector.length; i++) {
+    media.push(vector[i])
+  }
+  media / (i + 1) = result 
+  console.log(result)
+}
