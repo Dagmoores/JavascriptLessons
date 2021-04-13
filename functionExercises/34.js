01fc025089b85be7bab056ee9b9cca0dfa521751
Daniel Moore <dagmoores@gmail.com>
2021-04-13 18:23:55 -0300

    Create 34.js

---
+++ functionExercises/34.js
@@ -0,0 +1,21 @@
+/* 34) Construa uma função que receberá duas Strings de tamanhos 
+variados e que retornará True ou False caso todos os caracteres
+(independentemente de ser maiúsculo ou minúsculo) estejam contidos em
+ambas palavras. */
+
+let string1 = "abc";
+let string2 = "ABC";
+let checker1 = [];
+let checker2 = [];
+
+function stringChecker(string1, string2) {
+  checker1 = string1.split("");
+  checker2 = string2.split("");
+  if(checker1 == checker2) {
+    true
+  } else {
+    false
+  }
+
+}
+console.log(string1.split())
