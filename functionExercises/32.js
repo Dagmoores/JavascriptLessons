c5822d68451dc9ef73e7ae87271ad406d4f916f8
Daniel Moore <dagmoores@gmail.com>
2021-04-13 17:39:31 -0300

    Update 32.js

--- functionExercises/32.js
+++ functionExercises/32.js
@@ -2,13 +2,15 @@
 de um vetor de inteiros. */ 
 
 let vector = [1, 3, 8, 10, 12, 89]
-let media
-let result
+let media = 0
+let result = 0
 
-function mediaCalc (vector[i]) {
+function mediaCalc(vector) {
   for (i = 0; i < vector.length; i++) {
-    media.push(vector[i])
+    media += vector[i]
   }
-  media / (i + 1) = result 
+  result = media / (i)
   console.log(result)
 }
+
+mediaCalc(vector)
