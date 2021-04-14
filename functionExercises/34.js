271d61aed3ee2bad3917f17c02a342c01df014d8
Daniel Moore <dagmoores@gmail.com>
2021-04-14 12:17:57 -0300

    Update 34.js

--- functionExercises/34.js
+++ functionExercises/34.js
@@ -3,13 +3,13 @@ variados e que retornará True ou False caso todos os caracteres
 (independentemente de ser maiúsculo ou minúsculo) estejam contidos em
 ambas palavras. */
 
-
+let tester;
 
 function stringChecker(string1, string2) {
   
 string1 = "abc";
-string2 = "ABC";
-p = 0; 
+string2 = "ABCh";
+
 splited1 = [];
 splited2 = [];
   
@@ -19,20 +19,17 @@ splited2 = [];
   splited2 = string2.split("");
 
     for(i = 0; i < splited1.length; i++) {
-    console.log("valor do i fora", i)
-    console.log("valor do p fora", p)
-    while(splited1[i] == splited2[p]) {
-    p++
-      console.log("valor do i", i)
-      console.log("valor do p", p)
-    }
-    }   
-if (i != p) {
-  return false;
-} else {
-  return true;
+     for(p = 0; p < splited2.length; p++) {
+       if(splited1[i] == splited2[p]) {
+         tester = true;
+       } else {
+         tester = false;
+       }
+     }
+    }  
+  return tester;
 }
 
-}
+
 
 stringChecker();
