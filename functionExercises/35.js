3b02891dffd64c60b74389f651b0f62841737d0e
Daniel Moore <dagmoores@gmail.com>
2021-04-14 13:26:23 -0300

    Update 35.js

--- functionExercises/35.js
+++ functionExercises/35.js
@@ -1,17 +1,17 @@
-/* 35) Crie dois vetores chamados vetorPilha e vetorAdiciona. Inicialmente,
-o vetorPilha conterá cinco elementos inteiros: [1, 2, 3, 4, 5]. Você 
-deverá adicionar os valores contidos no vetorAdiciona [6, 7, 8, 9, 10] 
-ao vetor pilha e mostrá-los no console. É importante lembrar que o 
-método Push retorna somente o tamanho do Vetor. Ao final das operações 
-imprima os vetores no console. */
+/* 36) Crie duas funções que recebem dois parâmetros, um vetor com apenas 
+valores numéricos e um número inteiro. Faça com que a primeira função 
+retorne outro vetor que será resultado da multiplicação de cada elemento
+pelo número passado como parâmetro. A segunda função fará o mesmo da 
+primeira se e somente se o valor do elemento for maior que 5. */
 
-let vectorStack = [1, 2, 3, 4, 5];
-let vectorAdd = [6, 7, 8, 9, 10];
+let vector = [1, 2, 3, 4, 5];
+let vectorResult =[5];
+let number;
 
-console.log(vectorStack);
-
-for (i = 0; i < vectorAdd.length; i++) {
-  vectorStack.push(vectorAdd[i])
+function multiply1 (vector, number) {
+  for(i = 0; i < vector.length; i++){ 
+  vectorResult.push(vector[i] * number)
+  }
 }
 
-console.log(vectorStack)
+console.log(vectorResult)
