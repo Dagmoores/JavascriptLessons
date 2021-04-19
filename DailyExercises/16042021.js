4d2060ad77fa95cc3b5543607ccc00851a0fb9f6
Daniel Moore <dagmoores@gmail.com>
2021-04-19 16:13:28 -0300

    Create 16042021.js

---
+++ DailyExercises/16042021.js
@@ -0,0 +1,23 @@
+/* DESAFIO DO DIA 16/04/2021 - Sexta-feira
+Nível de dificuldade:  ⭐⭐
+Faça um programa para identificar se 5 números são primos.
+
+Número primo, é um número natural, maior que 1, apenas divisível 
+por si próprio e pela unidade. */
+
+
+
+function testPrime () {
+let numbers = [1, 2, 17, 13, 15]
+
+for (i = 0; i < numbers.length; i++) {
+  for(p = 2; p <= 7; p++) {
+    if (numbers[i] % p == 0) {
+      numbers.pop(numbers[i])
+    } 
+  } 
+}
+  console.log(numbers)
+}
+
+testPrime()
