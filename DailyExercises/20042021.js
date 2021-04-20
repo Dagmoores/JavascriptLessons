f5b6fc8f15a0150986942fc2c50ecf239032a054
Daniel Moore <dagmoores@gmail.com>
2021-04-20 14:47:20 -0300

    Create 20042021.js

---
+++ DailyExercises/20042021.js
@@ -0,0 +1,13 @@
+/* DESAFIO DO DIA 20/04/21 - VALIDAÇÃO DE NOME DE USUÁRIO
+
+    Nível de dificuldade: ⭐⭐
+Faça com que a função  validaUsuario (  str ) pegue o parâmetro 
+str que será informado pelo usuário e determine se é um nome de
+usuário válido de acordo com as seguintes regras:
+        1. O nome de usuário tem entre 4 e 25 caracteres.
+         2. Deve começar com uma letra.
+         3. Ele só pode conter letras, números ou underline.
+         4. Opcional: Não pode terminar com um underline. 
+
+Se o nome de usuário for válido, seu programa deve retornar a 
+string  true , caso contrário, retornar a string  false. */
