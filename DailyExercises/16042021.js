cc6b9d1b0ba795fdea2528fa4391bc400c0452bc
Daniel Moore <dagmoores@gmail.com>
2021-04-19 23:49:18 -0300

    Update 16042021.js

--- DailyExercises/16042021.js
+++ DailyExercises/16042021.js
@@ -5,43 +5,29 @@ Faça um programa para identificar se 5 números são primos.
 Número primo, é um número natural, maior que 1, apenas divisível 
 por si próprio e pela unidade. */
 
-//Unfinished
+
 
 function testPrime () {
     let numbers = [17, 2, 1, 13, 15]
     let primeNumbers = [];
+    let tester;
+    
     
     for (i = 0; i < numbers.length; i++) {
+        
+    tester = true;
+
         for (d = 2; d <= 7; d++) {
-            if (numbers[i] > 1 && numbers[i] % d != 0 
-                && numbers[i] != primeNumbers[primeNumbers.length - 1]) {
 
-                primeNumbers.push(numbers[i]);
-
-console.log("push - ", "i:", i, "Numbers[i]:", numbers[i], "primeNumbers:", primeNumbers, "d:", d,
-"last Prime Number:", primeNumbers[primeNumbers.length - 2]);
-   
-            } else if (numbers[i] % d == 0 || numbers[i] % (d - 1) == 0) {
-
-console.log("pop1 -  ", "i:", i, "Numbers[i]:", numbers[i], "primeNumbers:", primeNumbers, "d:", d,
-"last Prime Number:", primeNumbers[primeNumbers.length - 2]);
-
-                primeNumbers.pop();
-                i++;
-
-
-            } else if (numbers[i] == primeNumbers[primeNumbers.length - 2]) {
-
-
-console.log("pop2 -  ", "i:", i, "Numbers[i]:", numbers[i], "primeNumbers:", primeNumbers, "d:", d,
-"last Prime Number:", primeNumbers[primeNumbers.length - 2]);
-                
-                primeNumbers.pop();
-                i++;
-            }
+            if (numbers[i] <= 1 || numbers[i] % d == 0) {
+                tester = false;
         }
+    } 
+    if (tester == true) {
+        primeNumbers.push(numbers[i])
+         } 
     }
     console.log(primeNumbers)
-    } 
-    
-    testPrime();
+}
+
+testPrime();
