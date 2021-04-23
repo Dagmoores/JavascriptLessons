e76fbeb84bec0b4e4616ca81e2e6cb9a56f1bc1e
Daniel Moore <dagmoores@gmail.com>
2021-04-23 19:12:30 -0300

    Create 23042021.js

---
+++ DailyExercises/23042021.js
@@ -0,0 +1,18 @@
+/* DESAFIO DO DIA 23/04/21 - INVERTENDO STRINGS
+Nível de dificuldade: ⭐ (Retirado de http://devfuria.com.br/)
+Faça um programa que inverta uma string, exemplo: “animal” em 
+“lamina”.
+
+Dica: Se achar o desafio muito fácil, tente encontrar 
+outras alternativas de resolvê-lo. */
+
+//Unfinished
+
+function rewrite(word) {
+
+   let arrayWord = word.split("")
+   console.log(arrayWord);
+
+};
+
+rewrite("pessego")
