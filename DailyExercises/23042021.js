ef026d316ebceb4797145067a1f12ddd811ac5c9
Daniel Moore <dagmoores@gmail.com>
2021-04-23 21:20:22 -0300

    Update 23042021.js

--- DailyExercises/23042021.js
+++ DailyExercises/23042021.js
@@ -2,7 +2,6 @@
 Nível de dificuldade: ⭐ (Retirado de http://devfuria.com.br/)
 Faça um programa que inverta uma string, exemplo: “animal” em 
 “lamina”.
-
 Dica: Se achar o desafio muito fácil, tente encontrar 
 outras alternativas de resolvê-lo. */
 
@@ -10,15 +9,18 @@ outras alternativas de resolvê-lo. */
 
 function rewrite(word) {
 
-   let arrayWord = word.split("");
-   let tempArray = [];
+let arrayWord = word.split("");
+let tempArray = [];
 
-   for (i = 0; i < arrayWord.length; i++) {
-    tempArray[i] = arrayWord[arrayWord.length];
-    arrayWord.pop();
-    console.log(tempArray);
-   }
+console.log(word);
 
-};
+    for (i = 0; i < arrayWord.length; i++) {
+        
+     tempArray[i] = arrayWord[arrayWord.length - i -1];
+    };
 
-rewrite("pessego")
+
+    console.log(tempArray.join("").toString())
+ };
+ 
+ rewrite("pessego")
