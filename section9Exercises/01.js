201123faa8a9ddbc3408217d11fcddef93103f52
Daniel Moore <dagmoores@gmail.com>
2021-07-23 18:49:50 -0300

    Update 01.js

--- section9Exercises/01.js
+++ section9Exercises/01.js
@@ -1,11 +1,12 @@
-Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+/* Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
 função) e com ponto de exclamação "!" no final.
 Exemplos:
 cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
-cumprimentar("Maria") // retornará "Olá, Maria!"
+cumprimentar("Maria") // retornará "Olá, Maria!" */
 
 function retornarStringConcat (palavra) {
 	return console.log(`Olá, ${palavra}!`)
 }
 
 retornarStringConcat('Daniel')
+retornarStringConcat('Mundo')
