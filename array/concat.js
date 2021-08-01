2b8a957da46c5a2f3e4ce1656cb48a6c1b7385e7
Daniel Moore <dagmoores@gmail.com>
2021-07-31 22:07:23 -0300

    Add files via upload

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Valesca', 'Cibalena']
+const filhos = ['Washinton', 'Wescley']
+
+const todos = filhas.concat(filhos, 'fulanos')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 5], 6, [[7, 8]]))
