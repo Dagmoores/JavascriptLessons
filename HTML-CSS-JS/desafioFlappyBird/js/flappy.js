8db4843b41c72c86084dd832f68546a5b6a98479
Daniel Moore <dagmoores@gmail.com>
2022-01-12 10:37:49 -0300

    Update flappy.js

--- HTML-CSS-JS/desafioFlappyBird/js/flappy.js
+++ HTML-CSS-JS/desafioFlappyBird/js/flappy.js
@@ -1,1 +1,48 @@
-//javascript
+const div = document.querySelector("div")
+const coluna = document.querySelectorAll("[coluna]")
+const borda = document.querySelectorAll("[borda]")
+
+
+
+
+function iniciar() {
+    coluna.forEach(e => e.style.right = "18%")
+}
+
+function checarPosicao(e) {
+    let rightNumerico = parseInt(e.style.right)
+    if (rightNumerico > 70) {
+        e.style.right = "18%"      
+    }
+} 
+
+
+div.onclick = e => {
+
+
+    //Inicio
+    iniciar()
+    //Fim-do-Inicio
+
+    //Mecanica
+    setInterval(function() { 
+
+    coluna.forEach(e => checarPosicao(e))
+
+    coluna.forEach(e => {
+
+        let rightNumerico = parseInt(e.style.right)
+        rightNumerico += 5
+        e.style.right = `${rightNumerico}%`
+        
+    
+
+        
+
+
+        }
+        )       
+    }
+    
+    , 100)
+}   
