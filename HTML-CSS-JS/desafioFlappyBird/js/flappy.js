c7819a44c8d92da4580f2414a23478713fccfb2a
Daniel Moore <dagmoores@gmail.com>
2022-01-12 21:05:02 -0300

    Update flappy.js

--- HTML-CSS-JS/desafioFlappyBird/js/flappy.js
+++ HTML-CSS-JS/desafioFlappyBird/js/flappy.js
@@ -1,6 +1,8 @@
 const div = document.querySelector("div")
 const coluna = document.querySelectorAll("[coluna]")
 const borda = document.querySelectorAll("[borda]")
+const novaDivPai = document.createElement('div')
+const novaDivFilho = document.createElement('div')
 
 
 
@@ -9,12 +11,18 @@ function iniciar() {
     coluna.forEach(e => e.style.right = "18%")
 }
 
-function checarPosicao(e) {
-    let rightNumerico = parseInt(e.style.right)
-    if (rightNumerico > 70) {
-        e.style.right = "18%"      
-    }
-} 
+
+
+	//função criar div cima
+	function criarDivCima() {
+	novaDivPai.className = 'coluna-cima'
+	novaDivPai.setAttribute('coluna', '')
+	div.appendChild(novaDivPai)
+	novaDivPai.appendChild(novaDivFilho)
+	novaDivFilho.className = 'borda-coluna-cima'
+	}
+
+
 
 
 div.onclick = e => {
@@ -22,27 +30,36 @@ div.onclick = e => {
 
     //Inicio
     iniciar()
-    //Fim-do-Inicio
+	
+
+	
 
     //Mecanica
     setInterval(function() { 
 
-    coluna.forEach(e => checarPosicao(e))
+    // coluna.forEach(e => checarPosicao(e))
 
     coluna.forEach(e => {
 
+		//Movimento das colunas
         let rightNumerico = parseInt(e.style.right)
         rightNumerico += 5
         e.style.right = `${rightNumerico}%`
-        
-    
-
-        
-
-
-        }
-        )       
-    }
-    
-    , 100)
+	
+		//desaparecimento e surgimento de colunas
+		if (rightNumerico > 75) {
+			e.style.display = "none"
+		}
+		
+		if(rightNumerico == 53) {
+			criarDivCima()
+			
+			
+			}
+	
+		console.log(rightNumerico)
+			}
+		)       
+		}
+		, 300)
 }   
