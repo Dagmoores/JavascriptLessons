4613a7a3ae70082b66c8f1da9c460e8f400fa4f9
Dagmoores <dagmoores@gmail.com>
2022-03-10 11:45:19 -0300

    inicio mudanca no arquivo webpack.config.js

--- bootstrap/bootstrap/projeto-galeria/webpack.config.js
+++ bootstrap/bootstrap/projeto-galeria/webpack.config.js
@@ -1,53 +1,21 @@
-const modoDev = process.env.NODE_ENV !== 'production'
+const path = require('path')
 const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    mode: modoDev ? 'development' : 'production',
-    entry: './src/index.js',
-    devServer: {
-        contentBase: './build',
-        port: 9000,
-    },
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true
-            }),
-            new OptimizeCSSAssetsPlugin({})
-        ]
-    },
-    output: {
-        filename: 'app.js',
-        path: __dirname + '/build'
-    },
-    plugins: [
-        new MiniCssExtractPlugin({ filename: 'estilo.css' }),
-        new CopyWebpackPlugin([
-            { context: 'src/', from: '**/*.html' },
-            { context: 'src/', from: 'imgs/**/*' }
-        ])
-    ],
+    mode: 'development',
     module: {
         rules: [{
-            test: /\.s?[ac]ss$/,
+            test: '/\.s[ac]ss$/i',
             use: [
-                MiniCssExtractPlugin.loader,
-                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader', // interpreta @import, url()...
-                'sass-loader',
+                "sass-loader"
             ]
-        }, {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: ['file-loader']
-        }, {
-            test: /.(ttf|otf|eot|svg|woff(2)?)$/,
-            use: ['file-loader']
         }]
-    }
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname + 'src/pages/template')
+        },
+        compress: true,
+        port: 8080
+    }   
 }
