cf4d15c05719043e616ae2cbea79122bd0e97a7d
Dagmoores <dagmoores@gmail.com>
2022-03-18 11:40:55 -0300

    arquivo includes tem alguns erros esperados, pois esta sendo testado o uso do each e como o jquery faz um wrap nos nodes do html

--- bootstrap/bootstrap/projeto-galeria/src/js/core/includes.js
+++ bootstrap/bootstrap/projeto-galeria/src/js/core/includes.js
@@ -1,23 +1,23 @@
 import $ from 'jquery'
 
-const loadHtmlSuccessCallbacks = []
-
-export function onLoadHtmlSuccess(callback) {
-    if(!loadHtmlSuccessCallbacks.includes(callback)) {
-        loadHtmlSuccessCallbacks.push(callback)
-    }
-}
 
 function loadIncludes(parent) {
-    if(!parent) parent = 'body'
+    if(!parent) {parent = 'body'}
+
     $(parent).find('[wm-include]').each(function(i, e) {
+
+        console.log(e)
+    )
         const url = $(e).attr('wm-include')
-        console.log(url)
+        const url2 = $('body').get(0)
+        // const url2 = $('body').attr('dasdsa').get(0)
+        
         $.ajax({
             url,
             success(data) {
                 $(e).html(data)
                 $(e).removeAttr('wm-include')
+                console.log('aqui', url, url2)
 
                 loadHtmlSuccessCallbacks.forEach(
                     callback => callback(data))
